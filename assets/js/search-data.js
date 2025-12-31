79ec668265338c3b425fe56d24d045da3908afb3
mohamadH80 <84089279+mohamadH80@users.noreply.github.com>
2025-12-31 13:06:33 +0000

    Deploying to gh-pages from @ mohamadH80/mohamadH80.github.io@3b363d2732e413716d873cdbc6036ea1c3b16402 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,12 +451,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{
+          section: "News",},{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
