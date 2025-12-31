066c95ed6adb735e5135d5d015399af58bd372bd
mohamadH80 <84089279+mohamadH80@users.noreply.github.com>
2025-12-31 13:05:31 +0000

    Deploying to gh-pages from @ mohamadH80/mohamadH80.github.io@5b42dc749b3e3a73bc835d84774488aa632b35ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,11 +456,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
